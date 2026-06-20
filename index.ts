57da4e5437cb825d179d1f4be231185fced3f904
naims <naimdrive6@gmail.com>
2026-06-20 06:43:49 +0600

    remove all the dependencies.

--- index.ts
+++ index.ts
@@ -1,18 +1,6 @@
 import express, { Request, Response } from "express";
-import fs from "fs";
+
 const app = express();
-
-import swaggerUi from "swagger-ui-express";
-import YAML from "yaml";
-
-const file = fs.readFileSync("./swagger.yml", "utf8");
-const swaggerDocument = YAML.parse(file);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
