527305d9606a2eb795ffb739c4f6530b98be0477
naims <naimdrive6@gmail.com>
2026-06-20 09:02:41 +0600

    tutorial done.

---
+++ STEPS.md
@@ -0,0 +1,11 @@
+## 1. API Documentation
+
+## 2. YAML vs JSON
+
+## 3. What is Swagger
+
+## 4. Make our First OpenApi File
+
+## 5. Request body
+
+## 6. Response  
--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
-
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
+import fs from "fs";
 const app = express();
+
+const file = fs.readFileSync("./swagger.yml", "utf-8");
+const swaggerDocument = YAML.parse(file);
+
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
